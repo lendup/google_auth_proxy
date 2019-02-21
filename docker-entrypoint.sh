4cd90f0879659de8e82ecd88d6e2b58a6e2d21f4
mgraf <matthew.graf@lendup.com>
2019-02-20 16:55:17 -0800

    Add params to google_auth_proxy command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,11 @@
 
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
     # If no args given, or first argument starts with '-', run our main app w/ args
-    # TODO: convert env vars into options
-    exec "google_auth_proxy" "$@"
+    exec "google_auth_proxy" \
+        "--redirect-url=$GOOGLE_AUTH_PROXY_REDIRECT_URL" \
+        "--upstream=$GOOGLE_AUTH_PROXY_UPSTREAM" "--google-apps-domain=$GOOGLE_AUTH_PROXY_GOOGLE_APPS_DOMAIN" \
+        "--cookie-secret=$GOOGLE_AUTH_PROXY_COOKIE_SECRET" "--client-secret=$GOOGLE_AUTH_PROXY_CLIENT_SECRET" \
+        "--client-id=$GOOGLE_AUTH_PROXY_CLIENT_ID" "--pass-basic-auth=$GOOGLE_AUTH_PROXY_PASS_BASIC_AUTH"
 else
     # Running some other command
     exec "$@"
